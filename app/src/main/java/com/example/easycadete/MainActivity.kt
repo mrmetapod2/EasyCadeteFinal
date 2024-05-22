de014ef0f0ae78427dec9c79956c1870024c991d
mrmetapod2 <hectormzacrias@gmail.com>
2024-05-22 01:27:15 -0300

    iniciando con el registrar usuarios

--- app/src/main/java/com/example/easycadete/MainActivity.kt
+++ app/src/main/java/com/example/easycadete/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.easycadete
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,13 +19,18 @@ class MainActivity : AppCompatActivity() {
         //generando las variables de los elementos de la pantalla de login
         val Usuario= findViewById<EditText>(R.id.editTextText)
         val Contraseña= findViewById<EditText>(R.id.editTextTextPassword)
-        val Boton= findViewById<Button>(R.id.button)
+        val Ingresar= findViewById<Button>(R.id.button)
+        val RegistrarBut= findViewById<Button>(R.id.button3)
         val nivelFuncion = NivelFuncion()
 
 
         //reaccion al apretar un boton
-        Boton.setOnClickListener {
+        Ingresar.setOnClickListener {
             nivelFuncion.VerificacionUsuarios(this,Usuario.getText().toString(), Contraseña.getText().toString())
+        }
+        RegistrarBut.setOnClickListener {
+            val i = Intent(this, Registrar::class.java)
+            ContextCompat.startActivity(this, i, null)
         }
 
 
