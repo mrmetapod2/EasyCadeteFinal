c018c286b7a50cda76523c2eee2c569d97acb816
Gonzalo <gonzalo_ortiz_95@hotmail.com>
2024-05-31 01:10:00 -0300

    Nueva actualizacion de Solicitud entrega-GO

---
+++ app/src/main/java/Solicitud_Entrega/Solicitud.kt
@@ -0,0 +1,21 @@
+package Solicitud_Entrega
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.easycadete.R
+
+class Solicitud : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_solicitud)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
