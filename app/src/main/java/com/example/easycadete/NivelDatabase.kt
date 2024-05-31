b0c07b13f3d3d330a0e2c16f94e559d89c9d6350
mrmetapod2 <hectormzacrias@gmail.com>
2024-05-31 01:12:42 -0300

    añadi las tablas de usuario y cadete

--- app/src/main/java/com/example/easycadete/NivelDatabase.kt
+++ app/src/main/java/com/example/easycadete/NivelDatabase.kt
@@ -12,8 +12,8 @@ class BaseDeDatos(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, n
         companion object {
             //esto va a ser modificado para que sea exactamente la base de datos que usemos
          const val DATABASE_NAME = "example.db"
-         const val DATABASE_VERSION = 4
-         const val TABLE_NAME = "Personas"
+         const val DATABASE_VERSION = 5
+         const val TABLE_NAME = "Persona"
          const val COLUMN_ID = "id"
          const val COLUMN_NAME = "name"
             const val COLUMN_CONT = "contraseña"
@@ -24,7 +24,30 @@ class BaseDeDatos(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, n
                     "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "$COLUMN_NAME TEXT NOT NULL," +
                     "$CADETE_O_USUARIO TEXT NOT NULL," +
-                    "$COLUMN_CONT TEXT NOT NULL)"
+                    "$COLUMN_CONT TEXT NOT NULL);"+
+            /*"CREATE TABLE Persona (" +
+                    "ID INT PRIMARY KEY AUTOINCREMENT," +
+                    "Nombre VARCHAR(100) NOT NULL," +
+                    "Apellido VARCHAR(100) NOT NULL," +
+                    "Contraseña VARCHAR(100) NOT NULL," +
+                    "Edad INT NOT NULL," +
+                    "DNI VARCHAR(20) NOT NULL," +
+                    "Email VARCHAR(100) NOT NULL UNIQUE," +
+                    "Telefono VARCHAR(20))" +*/
+
+            "CREATE TABLE Usuario (" +
+                    "ID INT PRIMARY KEY IDENTITY(1,1)," +
+                    "ID_Persona INT NOT NULL," +
+                    "FOREIGN KEY (ID_Persona) REFERENCES Persona(ID)," +
+                    ")" +
+            "CREATE TABLE Cadete (" +
+                    "ID INT PRIMARY KEY IDENTITY(1,1)," +
+                    "ID_Persona INT NOT NULL," +
+                    "Disponibilidad BIT DEFAULT 0," +
+                    "FOREIGN KEY (ID_Persona) REFERENCES Persona(ID)" +
+                    ");"
+
+
     }
         //maneje de versiones
         override fun onCreate(db: SQLiteDatabase) {
@@ -109,7 +132,7 @@ class NivelDatabase(context: Context) {
         }
 
         //se crea una nueva linea en la base de datos en la tabla asignada con los valores declarados
-        val newRowId = db.insert("Personas", null, values)
+        val newRowId = db.insert("Persona", null, values)
         //se chequea si funciono
         if (newRowId != -1L) {
             println("result")
